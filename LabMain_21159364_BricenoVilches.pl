9fc096a4b305ab2e6bc66e31fd9622d85875903c
LemonPatt <patricio.briceno@usach.cl>
2023-10-31 23:04:39 -0300

    Main añadido con RF 2 y 3 de forma parcial

---
+++ LabMain_21159364_BricenoVilches.pl
@@ -0,0 +1,9 @@
+
+option(Code, Message , CodeLink, InitialFlowCode, Keywords, [Code, Message, CodeLink, InitialFlowCode, Keywords]).
+
+flow(Id, NameMessage, Options, [Id, NameMessage, Options]).
+
+%Añadir eliminacion de duplicados y documentacion
+%option(1,"1-viajar", 2,4, ["viajar", "turistear", "conocer"], O1),
+%option(2, "2 - estudiar", 4, 3, ["aprender", "perfeccionarme"], O2),
+%flow(1,"holaflow", [O1,O1,O2], F1).
