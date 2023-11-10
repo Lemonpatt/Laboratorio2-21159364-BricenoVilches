543c808abfd536a3ed6ef8663e1ba60b7e5b352f
LemonPatt <patricio.briceno@usach.cl>
2023-11-09 23:15:14 -0300

    RF 9 Añadido

--- labMain_21159364_BricenoVilches.pl
+++ labMain_21159364_BricenoVilches.pl
@@ -108,5 +108,108 @@ systemAddChatbot(System, Chatbot, NewSystem) :-
     getSystemFechaCreacion(System,FechaCreacion),
     NewSystem = [Name, CBLink, Users, LoggedUser, FechaCreacion, ChatbotsNoDuplicados].
 
+%RF 9: TDA system - modificador
+% Dom: System (list) X Username (string) X NewSystem (list)
+% Meta Principal: systemAddUser/3
+% Meta Secundaria: crearUser/2, getSystemUsers/2,
+% verificarUserRepetidos/4, getSystemName/2, getSystemInitialCBLink/2,
+% getSystemChatbots/2, getSystemLoggedUser/2,
+% getSystemFechaCreacion/2, NewSystem = [Name, CBLink, NewUsers,
+% LoggedUser, FechaCreacion, Chatbots].
+% Predicado modificador para añadir usuarios a un sistema, solo agrega
+% al usuario si el nombre de este no existe en el sistema.
+
+systemAddUser(System, Username, NewSystem):-
+    crearUser(Username, User),
+    getSystemUsers(System, Users),
+    verificarUserRepetidos([User|Users], [], [], NewUsers),
+    getSystemName(System, Name),
+    getSystemInitialCBLink(System, CBLink),
+    getSystemChatbots(System, Chatbots),
+    getSystemLoggedUser(System, LoggedUser),
+    getSystemFechaCreacion(System,FechaCreacion),
+    NewSystem = [Name, CBLink, NewUsers, LoggedUser, FechaCreacion, Chatbots].
+
+
+%RF 10: TDA system - modificador
+% Dom: System (list) X Username (string) X NewSystem (list)
+% Meta Principal: systemLogin/3
+systemLogin(System, Username, NewSystem):-
+    getSystemUsers(System, Users),
+    encontrarUser(Username, Users, UserEncontrado),
+    vacioLoggedUser(System), %Verifica que no haya nadie logeado
+    getSystemName(System, Name),
+    getSystemInitialCBLink(System, CBLink),
+    getSystemChatbots(System, Chatbots),
+    getSystemFechaCreacion(System,FechaCreacion),
+    NewSystem = [Name, CBLink, Users, UserEncontrado, FechaCreacion, Chatbots].
+
+
+%RF 11: TDA system - modificador
+% Dom: System (list) X NewSystem (list)
+% Meta Principal: systemLogout/2
+% Meta Secundaria: \+vacioLoggedUser/1, getSystemLoggedUser/2,
+% getSystemUsers/2, actualizarUsers/4, getSystemName/2,
+% getSystemInitialCBLink/2, getSystemChatbots/2,
+% getSystemFechaCreacion/2, NewSystem = [Name, CBLink, UsersActualizada, [], FechaCreacion, Chatbots]
+systemLogout(System, NewSystem):-
+    \+ vacioLoggedUser(System),
+    getSystemLoggedUser(System, UserActualizado),
+    getSystemUsers(System, Users),
+    actualizarUsers(UserActualizado, Users, [], UsersActualizada),
+    getSystemName(System, Name),
+    getSystemInitialCBLink(System, CBLink),
+    getSystemChatbots(System, Chatbots),
+    getSystemFechaCreacion(System,FechaCreacion),
+    NewSystem = [Name, CBLink, UsersActualizada, [], FechaCreacion, Chatbots].
+
+%RF 12: TDA system - otro
+% Dom: System (list) X Message (string) X NewSystem (list)
+% Meta Principal: systemTalkRec/3
+% Meta Secundaria:\+vacioLoggedUser/2, getSystemLoggedUser/2,
+% getSystemInitialCBLink/2, getSystemChatbots/2, encontrarChatbot/3,
+% getChatbotStartFlowId/2, getChatbotFlows/2, encontrarFlow/3,
+% getFlowOptions/2, encontrarMsgOp/3, crearChatHistory/5,
+% getOpChatbotCodeLink/2, getOpInitialFlowLink/2,
+% cambiarFlowCodeChatbot/3, getChatbotFlows/2, actualizarChatbots/4,
+% getUserName/2, actualizarNuevoSystem/5.
+%
+%
+systemTalkRec(System, Msg, NewSystem):-
+    \+ vacioLoggedUser(System), %Tiene que haber alguien logeado en el system
+    getSystemLoggedUser(System, LoggedUser),
+    getSystemInitialCBLink(System, CBLink),
+    getSystemChatbots(System, Chatbots),
+    encontrarChatbot(CBLink, Chatbots, ChatbotEncontrado),
+    getChatbotStartFlowId(ChatbotEncontrado, InitialFlowId),
+    getChatbotFlows(ChatbotEncontrado, Flows),
+    encontrarFlow(InitialFlowId, Flows, FlowEncontrado),
+    getFlowOptions(FlowEncontrado, Options),
+    encontrarMsgOp(Msg, Options, OptionEncontrada),
+    crearChatHistory(LoggedUser, Msg, ChatbotEncontrado, FlowEncontrado, ChatHistory),
+    getOpChatbotCodeLink(OptionEncontrada, CBLinkNew),
+    encontrarChatbot(CBLinkNew, Chatbots, ChatbotACambiar),
+    getOpInitialFlowCodeLink(OptionEncontrada, InitialFlowCodeLink),
+    cambiarFlowCodeChatbot(ChatbotACambiar, InitialFlowCodeLink, ChatbotNuevo),
+    getChatbotFlows(ChatbotNuevo, FlowsARevisar),
+    encontrarFlow(InitialFlowCodeLink, FlowsARevisar, _), %Revisa que el nuevo Chatbot tenga el Flow al cual ahora se dirige
+    actualizarChatbots(ChatbotNuevo, Chatbots, [], ChatbotsActualizados),
+    getUserName(LoggedUser, User),
+    actualizarNuevoSystem(System, CBLinkNew, User, ChatHistory, ChatbotsActualizados, NewSystem).
+
+
+
+
+%Añadir eliminacion de duplicados y documentacion
+% set_prolog_flag(answer_write_options,[max_depth(0)]),option(1,"1-viajar",2,1, ["viajar", "turistear", "conocer"],O1),option(2, "2 - estudiar",4, 3, ["aprender", "perfeccionarme"], O2),flow(1,"flujo 1:mensaje de prueba", [O1], F1), flowAddOption(F1, O2, F2),flow(2, "Flujo 1: mensaje de prueba", [ ], F3),chatbot(1, "chatbot", "Prueba1", 1, [F1], CB1), chatbotAddFlow(CB1, F3, CB2),chatbot(2, "chatbot Viajes", "Prueba2", 1, [F2], CB3),system("Sistema1", 1,[CB2], S1),systemAddChatbot(S1, CB3, S2), systemAddUser(S2, "user0", S3), systemAddUser(S3, "user1", S4),systemLogin(S3, "user1", S5), systemLogout(S5,S6).
+%
+%
+%
+%
+%
+% set_prolog_flag(answer_write_options,[max_depth(0)]),option(1,"1-viajar",2,1, ["viajar", "turistear", "conocer"],O1),option(2, "2-estudiar",4, 3, ["aprender", "perfeccionarme"], O2),flow(1,"flujo 1:mensaje de prueba", [O1], F1), flowAddOption(F1, O2, F2),flow(2, "Flujo 1: mensaje de prueba", [ ], F3),chatbot(1, "chatbot", "Prueba1", 1, [F2], CB1), chatbotAddFlow(CB1, F3, CB2),chatbot(2, "chatbot Viajes", "Prueba2", 1, [F2], CB3),system("Sistema1", 1,[CB2], S1),systemAddChatbot(S1, CB3, S2), systemAddUser(S2, "user0", S3), systemAddUser(S3, "user1", S4),systemLogin(S4, "user1", S5), systemLogout(S5,S6),systemTalkRec(S5, "Turistear", S7).
+
+%Funciones auxiliares que tendrán su propio archivo.
+
 
 
